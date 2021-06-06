9324bb3f19b5adcdff5f8a37fc4ac00dde212710
vhuang5564 <vhuang5564@gmail.com>
2021-06-06 06:53:54 +0000

    Add more conditionals examples

--- conditionals.js
+++ conditionals.js
@@ -19,7 +19,7 @@ if (cold) {
   console.log("Short sleeves are fine.");
 } */
 
-const temperature = 16;
+/*const temperature = 16;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -29,4 +29,24 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
-console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!");*/
+
+
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+const temperature = 30;
+if (temperature < -40 || temperature > 40) {
+    console.log("Maybe going outside isn't such a great idea…");
+  }
+
+const raining = false;
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
