f4252bfcd1095ae589e343bb650e4fa81a280c39
vhuang5564 <vhuang5564@gmail.com>
2021-06-10 19:01:05 +0000

    listing and reversing pack

---
+++ ingredients.js
@@ -0,0 +1,21 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+
+// Write a for loop that prints out the contents of ingredients:
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+for (let i = (ingredients.length - 1); i >= 0; i--) {
+  console.log(ingredients[i]);
+}
